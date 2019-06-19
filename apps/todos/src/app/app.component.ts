2605cc3f32ac49ecb4c9b0986fdef3ff802c304d
Mike Richard <mrichard@ea.com>
2019-06-19 13:24:32 -0700

    Added in a new "data" lib to have the Todo interface.  Updated to use this new interface.

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Todo {
-  title: string;
-}
+import { Todo } from '@nx-tutorial/data';
 
 @Component({
   selector: 'nx-tutorial-root',
